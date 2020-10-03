e7b7a2b4a520e24b19c316218d2c109fc48e500a
proninserge <60112381+proninserge@users.noreply.github.com>
2020-10-03 16:57:19 +0500

    интеграция react router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter, Link} from 'react-router-dom';
 import MainScreen from '../main-screen/main-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
+import OfferScreen from '../offer-screen/offer-screen';
+import SignInScreen from '../sign-in-screen/sign-in-screen';
 
 
 const App = (props) => {
   const {offersCount} = props;
 
   return (
-    <MainScreen offersCount={offersCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen offersCount={offersCount} />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesScreen />
+        </Route>
+        <Route exact path="/login">
+          <SignInScreen />
+        </Route>
+        <Route exact path="/offer/:id">
+          <OfferScreen />
+        </Route>
+        <Route
+          render={() => (
+            <Fragment>
+              <h1 style={{textAlign: `center`}}>
+                404.
+                <br />
+                <small>Page not found</small>
+              </h1>
+              <Link style={{display: `block`, color: `blue`, width: `100%`, textAlign: `center`}} to="/">Go to main</Link>
+            </Fragment>
+          )}
+        />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
